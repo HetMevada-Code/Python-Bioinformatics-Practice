d48ad1a8bff3235f4040af58af159ae19a860b66
Het Mevada <hetmevada50@gmail.com>
2026-07-11 05:09:01 +0000

    add temp formulas

--- primer_melting_temp_estimator.py
+++ primer_melting_temp_estimator.py
@@ -13,12 +13,21 @@ Practice mathematical logic in Python, conditional statements, and counting stri
 primer_seq = input("Enter Your Primer Sequence: ").upper().strip()
 
 len_primer_seq = len(primer_seq)
-print(len_primer_seq)
+print("Your Primer Sequence Legth: ",len_primer_seq)
 
 A_count = primer_seq.count("A")
 T_count = primer_seq.count("T")
 G_count = primer_seq.count("G")
 C_count = primer_seq.count("C")
 
+# use Wallance Rule Formula for Primer length <= 14
+Tm_1 = 4 * (G_count + C_count) + 2 * (A_count + T_count)
 
+# use The Basic satl-adjusted Formula for Primer length > 14
+Tm_2 = 64.9 + (41 * ((G_count + C_count - 16.4) // (len_primer_seq)) )
+
+if(len_primer_seq <= 14):
+    print("Your Sequence is less than 14 ntd. So, Melting Temperature by Wallance Rule Formula is", Tm_1)
+else:
+    print("Your Sequence is Greater than 14 ntd. So, Melting Temperature by The Basic salt-adjusted Fromula is", Tm_2)
 
