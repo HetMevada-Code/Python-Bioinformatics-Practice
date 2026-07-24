d00eecd8b9b7674f0c120a6156c3f90c3198bf03
Het Mevada <233040082+HetMevada-Code@users.noreply.github.com>
2026-07-24 14:48:47 +0530

    Implement list slicing to extract specific features
    Added a feature extraction example using list slicing.

---
+++ features.py
@@ -0,0 +1,7 @@
+"""
+Given a list of sequence features features = ["Promoter", "Exon1", "Intron", "Exon2", "Terminator"], 
+use list slicing to extract ["Exon1", "Intron", "Exon2"].
+"""
+
+features = ["Promoter", "Exon1", "Intron", "Exon2", "Terminator"]
+print(features[1 : 4])
