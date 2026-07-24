10cb1055298ac2d3da26fa64deb39fa0b7c8afc7
Het Mevada <233040082+HetMevada-Code@users.noreply.github.com>
2026-07-24 14:43:13 +0530

    Replace 'His' with 'Pro' in peptide sequence

---
+++ short_peptide_seq.py
@@ -0,0 +1,8 @@
+"""
+You have a list representing a short peptide sequence: peptide = ["Met", "Val", "His", "Leu"].
+By accessing the correct index, replace "His" with "Pro" to model a mutation.
+"""
+
+peptide = ["Met", "Val", "His", "Leu"]
+peptide[2] = "Pro"
+print(peptide)
